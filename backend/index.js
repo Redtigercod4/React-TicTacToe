8d0e8cb5586ac613d8851afd4a7d8393d7c34d58
Wes <76014409+Redtigercod4@users.noreply.github.com>
2022-04-05 14:19:21 +0100

    First Test passes

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,14 @@
 class TicTacToe {
+    constructor() {
+        this.game= {
+            player: 'X',
+            ai: 'O'
+        }
+    }
     
+    getGameMarkers() {
+        return this.game;
+    }
 }
 
 module.exports = { TicTacToe };
--- backend/index.test.js
+++ backend/index.test.js
@@ -1,10 +1,8 @@
-const { TicTacToe } = require('../index');
+const { TicTacToe } = require('./index');
 
 describe('TicTacToe logic', () => {
     it('If player is X, AI is O', () => {
-        const game = new TicTacToe();
-        game.Player = 'X';
-
-        expect(game.ai).toBe('O');
+        const game = new TicTacToe('X');
+        expect(game.getGameMarkers().ai).toBe('O');
     })
 })
