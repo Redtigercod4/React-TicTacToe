f133eca3d20116eaf1771daee35ef553da8cfded
Wes <76014409+Redtigercod4@users.noreply.github.com>
2022-04-06 11:09:25 +0100

    Updated tests and checks board

--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,34 @@
 class TicTacToe {
-    constructor() {
-        this.game= {
+    constructor(board) {
+        // Setting up the Players with set Markers
+        this.game = {
             player: 'X',
             ai: 'O'
         }
+
+        // Game Board setup as an array and will be given to the frontend via a GET Request
+        this.board = board;
     }
-    
+
+    // Returns the Markers for the Player and AI
     getGameMarkers() {
         return this.game;
+    }
+
+    // Returns the state of the game Board
+    getGameBoard() {
+        return this.board;
+    }
+
+    // Checks if the board has any spaces
+    checkForSpaces() {
+        for(let i = 0; i < 3; i++) {
+            for(let j = 0; i < 3; i++) {
+                if(this.board[i][j] === "") {
+                    return true;
+                } return  false;
+            }
+        }
     }
 }
 
--- backend/index.test.js
+++ backend/index.test.js
@@ -1,8 +1,50 @@
 const { TicTacToe } = require('./index');
 
 describe('TicTacToe logic', () => {
-    it('If player is X, AI is O', () => {
-        const game = new TicTacToe('X');
+    it('Checks if AI is O and Player is X', () => {
+        // Calls the TicTacToe Class
+        const game = new TicTacToe();
+
+        // Calls the getGameMarkers method and checks that the AI equals O
         expect(game.getGameMarkers().ai).toBe('O');
+
+        // Calls the getGameMarkers method and checks that the Player equals X
+        expect(game.getGameMarkers().player).toBe('X');
+    })
+
+    it('New Game should have empty board', () => {
+
+        // Creates an empty board to simulate a new game
+        let board = [["", "", ""], ["", "", ""], ["", "", ""]]
+
+        // Calls the TicTacToe Class and passes the board variable to the constructor
+        const game = new TicTacToe(board);
+
+        // Calls the getGameBoard method and checks that the board is empty
+        expect(game.getGameBoard()).toEqual([["", "", ""], ["", "", ""], ["", "", ""]]);
+    })
+
+    it('Checks Board for spaces with board being empty', () => {
+
+        // Creates an empty board to simulate a new game
+        let board = [["", "", ""], ["", "", ""], ["", "", ""]]
+
+        // Calls the TicTacToe Class and passes the board variable to the constructor
+        const game = new TicTacToe(board);
+
+        // Calls the checkForSpaces method and checks that the board has spaces
+        expect(game.checkForSpaces()).toBe(true);
+    })
+
+    it('Checks Board for spaces with board being full', () => {
+
+        // Creates a full board to simulate the end of a game
+        let board = [["X", "O", "X"], ["X", "O", "X"], ["X", "O", "X"]]
+
+        // Calls the TicTacToe Class and passes the board variable to the constructor
+        const game = new TicTacToe(board);
+
+        // Calls the checkForSpaces method and checks that the board has no spaces
+        expect(game.checkForSpaces()).toBe(false)
     })
 })
